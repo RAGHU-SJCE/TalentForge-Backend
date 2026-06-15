fd42782d9d5aa4578b076316a863c17a368c78ca
RAGHU-SJCE <rs2006.raghu@gmail.com>
2026-06-16 00:59:09 +0530

    chore: Configure SMTP port 587 for cloud compatibility

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,7 +4,9 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // Use false for port 587 (STARTTLS)
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
